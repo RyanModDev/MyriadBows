8e3f2691dd1bd79883df3fd81aec8157d14a99c2
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2022-08-17 19:10:12 +0800

    minor tweaks

--- src/main/java/dev/ryanccn/myriadbows/S2CPackets.java
+++ src/main/java/dev/ryanccn/myriadbows/S2CPackets.java
@@ -2,12 +2,14 @@ package dev.ryanccn.myriadbows;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.item.ItemStack;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.Identifier;
+
 import org.quiltmc.qsl.networking.api.PacketByteBufs;
 import org.quiltmc.qsl.networking.api.PacketSender;
 import org.quiltmc.qsl.networking.api.ServerPlayNetworking;
@@ -28,15 +30,11 @@ public final class S2CPackets {
 
     @Environment(EnvType.CLIENT)
     public void registerHandlers() {
-        // registers the packet handlers
-        // call this in your client entrypoint
         ClientPlayNetworking.registerGlobalReceiver(COMMS_CHANNEL, this::handlePacket);
     }
 
     @Environment(EnvType.CLIENT)
     private void handlePacket(MinecraftClient client, ClientPlayNetworkHandler handler, PacketByteBuf buf, PacketSender responseSender) {
-        // this is currently in a network worker thread
-        // read and parse packet data and do NOT do anything else here!
         final var data = buf.readItemStack();
 
         client.execute(() -> data.decrement(1));
--- src/main/java/dev/ryanccn/myriadbows/mixin/BowMixin.java
+++ src/main/java/dev/ryanccn/myriadbows/mixin/BowMixin.java
@@ -6,8 +6,8 @@ import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.BowItem;
 import net.minecraft.item.ItemStack;
-
 import net.minecraft.world.World;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
